25fe3059c53259b192f95225625a70a032cb8c0f
NicolasGrosjean <grosjen@hotmail.fr>
2017-07-09 22:24:56 +0200

    Parse characters

--- save_parser/myParser.py
+++ save_parser/myParser.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 20 21:12:34 2017
+Created on June 2017
 
 @author: Nicolas
 """
@@ -14,14 +14,27 @@ import pandas as pd
 saveDir = "../save_archiver/saves/"
 targetDir = "./results/"
 
+
+#%%
+
+extractedCharStats = list({"b_d", "d_d", "prs", "piety", "wealth", "emp",
+                           "host", "emi", "eyi", "rel", "cul", "bn"})
+charStatLib = {"b_d" : "Birth date", "d_d" : "Death date", "prs" :"Prestige",
+               "piety": "Piety", "wealth" : "Wealth", "emp" : "Employer",
+               "host" : "Host", "emi" : "Estimated month income",
+               "eyi" : "Estimated year income", "rel" : "Religion",
+               "cul" : "Culture", "bn" : "Birth Name"}
+
 #%%
 provinceKey = "provinces="
 variableKey = "variables="
 titleKey = "title="
+characterKey = "character="
 modifierToken = "modifier"
 
 provinceScope = "province"
 titleScope = "title"
+charScope = "character"
 
 #%%
 def unspaced(string):
@@ -39,17 +52,20 @@ def parseScope(it, init, n, scopeType):
     :param scopeType: name of the scope type
     :return: list of scope variables, modified it and init
     :rtype: (variable dictionnary, modifier dictionnary, iterator, int,
-             title type dictionnary)
+             title type dictionnary, character stats)
     """
     var = list()
     mod = list()
     titTyp = list()
+    charStats = list()
     i = init
     isProvince = (scopeType == provinceScope)
+    isCharacter = (scopeType == charScope)
 
     deep = 2 # deep 0 = root, 1 = provinces
-    isVariable = False
-    isTitle = False
+    isVariable = False # Is currently parsing a variable bloc
+    isTitle = False # Is currently parsing a title bloc
+    oneChar = None # Stats of oe character
     while (deep > 1) & (i < n):
         i += 1
         line = unspaced(next(it))
@@ -63,20 +79,37 @@ def parseScope(it, init, n, scopeType):
         if len(tokens) == 2:
             if deep == 2 :
                 scope = tokens[0]
+                if isCharacter :
+                    # Save previous character
+                    if oneChar != None:
+                        charStats.append(oneChar)
+                    # Prepare to store the next one
+                    oneChar = dict.fromkeys(list({scopeType}))
+                    oneChar[scopeType] = scope
+            
+            # Variable parsing
             if isVariable:
                 var.append({scopeType:scope, "variable":tokens[0],
                             "value":tokens[1]})
             if (deep == 3) & (variableKey in line):
                 isVariable = True
+              
+            # Modifiers parsing
             if (tokens[0] == modifierToken) & (tokens[1] != ''):
                 mod.append({scopeType:scope, "modifier":tokens[1]})
+            
+            # Title parsing
             if isProvince & (deep == 3) & (tokens[0].startswith("b_")) :
                 title = tokens[0]
-                isTitle = True
+                isTitle = True             
             if isTitle & (tokens[0] == "type"):
                 titTyp.append({provinceScope:scope, titleScope:title,
                                "type": tokens[1]})
-    return (var, mod, it, i, titTyp)
+                               
+            # Character parsing
+            if isCharacter & (tokens[0] in extractedCharStats):
+                oneChar[charStatLib[tokens[0]]] = tokens[1]
+    return (var, mod, it, i, titTyp, charStats)
 
 #%%
 
@@ -84,8 +117,9 @@ def parse(lines):
     """
     Parse the lines of a CK2 savegame file
     
-    :return: (provVar, provMod, titleVar)
+    :return: (provVar, provMod, titleVar, titTyp, charStats)
     """
+    characterFound = False
     provinceFound = False
     titleFound = False
     n = len(lines)
@@ -94,19 +128,28 @@ def parse(lines):
     while (not titleFound) & (i < n):
         i += 1
         line = next(it)
-        if provinceKey in line:
+        
+        if characterKey in line:
+           characterFound = True 
+           while ((not '{' in line) & (i < n)):
+                i += 1
+                line = next(it)
+           (charVar, charMod, it, i, empty, charStats) = parseScope(it, i, n, charScope)
+        
+        if characterFound & (provinceKey in line):
             provinceFound = True
             while ((not '{' in line) & (i < n)):
                 i += 1
                 line = next(it)
-            (provVar, provMod, it, i, titTyp) = parseScope(it, i, n, provinceScope)
+            (provVar, provMod, it, i, titTyp, empty) = parseScope(it, i, n, provinceScope)
+            
         if provinceFound & (titleKey in line):
             titleFound = True
             while ((not '{' in line) & (i < n)):
                 i += 1
                 line = next(it)
-            (titleVar, titleMod, it, i, empty) = parseScope(it, i, n, titleScope)
-    return (provVar, provMod, titleVar, titTyp)
+            (titleVar, titleMod, it, i, empty, empty) = parseScope(it, i, n, titleScope)
+    return (provVar, provMod, titleVar, titTyp, charStats)
 
 #%%
 
@@ -119,12 +162,14 @@ def getYearFromFileName(fileName):
     return None
     
 #%%
-def createOrConcatDataFrame(dictionnary, df):
+def createOrConcatDataFrame(dictionnary, df, year):
     """
     Create a dataframe or concatenate with df from a dictionnary
     
     :param dictionnary:
-    :param df: if len(df) == 0 then create the dataframe, otherwise concatenate with it
+    :param df: if len(df) == 0 then create the dataframe,
+                otherwise concatenate the dictionnary to df
+    :param year: Added year information to the dictionnary
     :return: dataframe
     """
     dfYear = pd.DataFrame(dictionnary)
@@ -146,6 +191,7 @@ print("{} files to parse".format(len(filesToParse)))
 dfProvVar = pd.DataFrame()
 dfProvMod = pd.DataFrame()
 dfTitleVar = pd.DataFrame()
+dfCharStats = pd.DataFrame()
 for fileName in filesToParse:
     # Get the year
     year = getYearFromFileName(fileName)
@@ -158,12 +204,14 @@ for fileName in filesToParse:
     readFile.close()
     
     # Parse
-    (provVar, provMod, titleVar, titleTyp) = parse(lines)
+    (provVar, provMod, titleVar, titleTyp, charStats) = parse(lines)
     
     # Data consolidation
-    dfProvVar = createOrConcatDataFrame(provVar, dfProvVar)
-    dfProvMod = createOrConcatDataFrame(provMod, dfProvMod)
-    dfTitleVar = createOrConcatDataFrame(titleVar, dfTitleVar)
+    dfProvVar = createOrConcatDataFrame(provVar, dfProvVar, year)
+    dfProvMod = createOrConcatDataFrame(provMod, dfProvMod, year)
+    dfTitleVar = createOrConcatDataFrame(titleVar, dfTitleVar, year)
+    dfCharStats = createOrConcatDataFrame(charStats, dfCharStats, year)
+    
     
     # We only need the last values because it does not change
     # In fact some tribal titles can become castle, city or temple
@@ -185,4 +233,5 @@ dfProvVar.to_csv(targetDir + "ProvinceVariables.csv", index=False)
 dfProvMod.to_csv(targetDir + "ProvinceModifiers.csv", index=False)
 dfTitleVar.to_csv(targetDir + "TitleVariables.csv", index=False)
 dfTitleTyp.to_csv(targetDir + "TitleTypes.csv", index=False)
+dfCharStats.to_csv(targetDir + "CharacterStats.csv", index=False, encoding='utf-8')
 
